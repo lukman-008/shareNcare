1043ce8acdbb54106411e0805eb574ea58ab71f7
Sreyans1998 <sreyansranjan1998@gmail.com>
2022-12-24 20:57:18 +0530

    added validator

--- lib/books.dart
+++ lib/books.dart
@@ -42,204 +42,207 @@ class _booksState extends State<books> {
       body: Container(
         padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
         child: SingleChildScrollView(
-          child: Column(children: [
-            SizedBox(
-              height: 30,
-              child: Text(
-                "PERSONAL DETAIL",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return ("This Field Can't be Empty");
-                }
-              },
-              controller: name,
-              decoration: InputDecoration(
-                  hintText: "NAME",
-                  labelText: "ADD NAME/ORG. NAME",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-              autofocus: false,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return ("This Field Can't be Empty");
-                }
-              },
-              controller: contact,
-              decoration: InputDecoration(
-                  hintText: "CONTACT NUMBER",
-                  labelText: "CONTACT NUMBER",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-              autofocus: false,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return ("Please Enter Your Email");
-                }
-
-                if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                    .hasMatch(value)) {
-                  return ("Please Enter a Valid Email");
-                }
-                return null;
-              },
-              controller: email,
-              decoration: InputDecoration(
-                  hintText: "EMAIL ADDRESS",
-                  labelText: "EMAIL ADDRESS",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-              autofocus: false,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return ("This Field Can't be Empty");
-                }
-              },
-              controller: address,
-              minLines: 3,
-              maxLines: 20,
-              decoration: InputDecoration(
-                  hintText: "ADDRESS",
-                  labelText: "ADD YOUR ADDRESS",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-              autofocus: false,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
+          child: Form(
+            key: _formkey,
+            child: Column(children: [
+              SizedBox(
                 height: 30,
                 child: Text(
-                  "ADD DETAILS",
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return ("This Field Can't be Empty");
-                }
-              },
-              controller: total,
-              decoration: InputDecoration(
-                  hintText: "ADD NUMBER OF BOOKS",
-                  labelText: "TOTAL BOOKS",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-              autofocus: false,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return ("This Field Can't be Empty");
-                }
-              },
-              controller: descr,
-              minLines: 3,
-              maxLines: 20,
-              keyboardType: TextInputType.multiline,
-              decoration: InputDecoration(
-                  hintText: "DESCRIPTION",
-                  labelText: "ADD DESCRIPTION OF ALL BOOKS",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-              autofocus: false,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.blue, Color.fromARGB(255, 3, 54, 96)]),
-                boxShadow: [BoxShadow(blurRadius: 20, color: Colors.white)],
-                color: Color.fromARGB(255, 3, 168, 244),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: MaterialButton(
-                onPressed: () {
-                  if (checkvalidation(_formkey) == false) {
-                    FirestoreHelper2.create((UserModel(
-                        name: name.text,
-                        contact: contact.text,
-                        email: email.text,
-                        address: address.text,
-                        total: total.text,
-                        descr: descr.text)));
-                  }
-                },
-                hoverColor: Colors.black,
-                padding: EdgeInsets.fromLTRB(70, 15, 70, 15),
-                child: Text(
-                  "ADD",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 255, 255, 255)),
+                  "PERSONAL DETAIL",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
-            ),
-          ]),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return ("This Field Can't be Empty");
+                  }
+                },
+                controller: name,
+                decoration: InputDecoration(
+                    hintText: "NAME",
+                    labelText: "ADD NAME/ORG. NAME",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                autofocus: false,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return ("This Field Can't be Empty");
+                  }
+                },
+                controller: contact,
+                decoration: InputDecoration(
+                    hintText: "CONTACT NUMBER",
+                    labelText: "CONTACT NUMBER",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                autofocus: false,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return ("Please Enter Your Email");
+                  }
+
+                  if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                      .hasMatch(value)) {
+                    return ("Please Enter a Valid Email");
+                  }
+                  return null;
+                },
+                controller: email,
+                decoration: InputDecoration(
+                    hintText: "EMAIL ADDRESS",
+                    labelText: "EMAIL ADDRESS",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                autofocus: false,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return ("This Field Can't be Empty");
+                  }
+                },
+                controller: address,
+                minLines: 3,
+                maxLines: 20,
+                decoration: InputDecoration(
+                    hintText: "ADDRESS",
+                    labelText: "ADD YOUR ADDRESS",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                autofocus: false,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                  height: 30,
+                  child: Text(
+                    "ADD DETAILS",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return ("This Field Can't be Empty");
+                  }
+                },
+                controller: total,
+                decoration: InputDecoration(
+                    hintText: "ADD NUMBER OF BOOKS",
+                    labelText: "TOTAL BOOKS",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                autofocus: false,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return ("This Field Can't be Empty");
+                  }
+                },
+                controller: descr,
+                minLines: 3,
+                maxLines: 20,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                    hintText: "DESCRIPTION",
+                    labelText: "ADD DESCRIPTION OF ALL BOOKS",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                autofocus: false,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.blue, Color.fromARGB(255, 3, 54, 96)]),
+                  boxShadow: [BoxShadow(blurRadius: 20, color: Colors.white)],
+                  color: Color.fromARGB(255, 3, 168, 244),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: MaterialButton(
+                  onPressed: () {
+                    if (checkvalidation(_formkey) == false) {
+                      FirestoreHelper2.create((UserModel(
+                          name: name.text,
+                          contact: contact.text,
+                          email: email.text,
+                          address: address.text,
+                          total: total.text,
+                          descr: descr.text)));
+                    }
+                  },
+                  hoverColor: Colors.black,
+                  padding: EdgeInsets.fromLTRB(70, 15, 70, 15),
+                  child: Text(
+                    "ADD",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                ),
+              ),
+            ]),
+          ),
         ),
       ),
     );
